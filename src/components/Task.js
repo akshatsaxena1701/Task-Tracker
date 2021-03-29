3126c44e617b79b1a1bc7d1bc3ded767669dce6a
Akshat Saxena <akshatsaxena1701@gmail.com>
2021-03-30 02:38:29 +0530

    d

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,7 @@ const Task = (props) => {
     return (
         <div className={`task ${props.task.reminder?'reminder':''}`} onDoubleClick={()=>props.onToggle(props.task.id)}>
             <h3>{props.task.text}
-             <FaTimes style={{color:"red",cursor:"pointer"}} onClick={()=>props.onDelete(props.task.id)}></FaTimes>
+             <FaTimes style={{color:"green",cursor:"pointer"}} onClick={()=>props.onDelete(props.task.id)}></FaTimes>
              </h3>
             <p>{props.task.day}</p>
             
